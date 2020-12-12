4c20ec436b111fd4223c17767c0f731e362ac326
Sneha Inguva <snehainguva@Snehas-MBP-2.attlocal.net>
2020-12-12 15:54:17 -0500

    initial framework for l7 uploadserver and socket server for uplaods

---
+++ cmd/l4upload/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+
+	// TODO(sneha): use flags for variables
+	addr := ":8080"
+	numWorkers := 10
+
+	// TODO (sneha): bit-shifting to read parts of a packet
+	// TODO (sneha): use buffered channel as a semphore for how many workers should be available
+
+	// NOTE: Under the hood this is making multiple syscalls (as are the calls to closer the listener and accept an incoming connection).
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("unable to open tcp listener on port: %s", addr)
+	}
+	defer func() {
+		fmt.Printf("closing listener on port: %s", addr)
+		listener.Close()
+	}()
+
+	conSema := make(chan struct{}, numWorkers)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("error accepting tcp connection: %s", addr)
+		}
+		conSema <- struct{}{}
+		go func(conn net.Conn) {
+			buff := make([]byte, 1500) // TODO (sneha): variable for buffer size
+			for {
+				fmt.Printf("accepting tcp connection from: %s", conn.RemoteAddr().String())
+				_, err := conn.Read(buff)
+				if err != nil {
+					// TODO(sneha): handle connection being closed differently (switch case for error types)
+					log.Printf("error reading from conn: %v", conn.RemoteAddr().String())
+					break
+				}
+				fmt.Println(string(buff))
+			}
+
+			// Defer to run before goroutine exits - release worker and close connection
+			defer func() {
+				fmt.Printf("closing tcp connection: %s", conn.RemoteAddr().String())
+				<-conSema
+				err := conn.Close()
+				if err != nil {
+					fmt.Printf("error closing connection for: %v", conn.RemoteAddr().String())
+				}
+			}()
+
+		}(conn)
+
+	}
+
+}
--- cmd/l7upload/main.go
+++ cmd/l7upload/main.go
@@ -1,6 +1,29 @@
 package main
 
+import "net/http"
+
+type healthHandler struct {
+}
+
+func (h *healthHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+
+}
+
+type uploadHandler struct {
+}
+
+func (h *uploadHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	// TODO(sneha): use object storage data store to hold data vs. saving on disk
+}
 
 func main() {
-	
+
+	// TODO(sneha): use flags for variables
+	addr := ":8080"
+
+	mux := http.NewServeMux()
+	mux.Handle("/health", &healthHandler{})
+	mux.Handle("/upload", &uploadHandler{})
+
+	http.ListenAndServe(addr, mux)
 }
